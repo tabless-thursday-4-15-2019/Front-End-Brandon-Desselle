71c12b48e3764be7115de6c0216c1f83f40ac0b4
Brandon Desselle <desselle.brandon2@gmail.com>
2019-04-15 17:27:18 -0500

    Update index.js

--- stash.it/src/index.js
+++ stash.it/src/index.js
@@ -1,6 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import LoginRegister, {
+  PROVIDER_GITHUB,
+  PROVIDER_GOOGLE
+} from 'react-mui-login-register';
+import Result from './Results';
+
+class App extends Component {
+  state = {
+    results: [],
+    disableLocal: false,
+    disableRegister: false
+
+  }
+
+  render() {
+    return (
+      <div>
+        <LoginRegister
+          onLogin={this.handleLogin}
+          onLoginWithProvider={this.handleLoginWithProvider}
+          onRegister={this.handleRegister}
+          onRegisterWithProvider={this.handleRegisterWithProvider}
+          providers={[PROVIDER_GITHUB, PROVIDER_GOOGLE]}
+          disableLocal={this.state.disableLocal}
+          disableRegister={this.state.disableRegister}
+        />
+
+        {this.state.results.map(r => <Result key={r} message={r} />)}
+
+      </div>
+    );
+  }
+
+  handleLogin = content => {
+    this.addResult(`Logging in with ${JSON.stringify(content)}`)
+  }
+  handleLoginWithProvider = provider => {
+    this.addResult(`Logging in with provider=${provider}`);
+  }
+  handleRegister = content => {
+    this.addResult(`Registering with ${JSON.stringify(content)}`)
+  }
+  handleRegisterWithProvider = provider => {
+    this.addResult(`Registering with provider=${provider}`);
+  }
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.checked });
+  };
+
+  addResult = msg => {
+    this.setState(prevState => {
+      return {
+        results: [...prevState.results, msg]
+      };
+    });
+  }
+}
+
+render(<App />, document.getElementById('root'));
+
+/* import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root')); */
