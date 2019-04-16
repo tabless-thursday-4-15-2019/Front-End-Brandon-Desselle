ed203a3bc8bef7e5b30bea4c5a856f1791eae0a8
Brandon Desselle <desselle.brandon2@gmail.com>
2019-04-16 12:03:46 -0500

    Update index.js

--- stash.it/src/index.js
+++ stash.it/src/index.js
@@ -1,70 +1,27 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+import thunk from 'redux-thunk'
+import reducer from './components/reducer/reducer'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-import LoginRegister, {
-  PROVIDER_GITHUB,
-  PROVIDER_GOOGLE
-} from 'react-mui-login-register';
-import Result from './Results';
+import './reset.css'
+import App from './App'
 
-class App extends Component {
-  state = {
-    results: [],
-    disableLocal: false,
-    disableRegister: false
+/* STORE */
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(thunk))
+)
 
-  }
-
-  render() {
-    return (
-      <div>
-        <LoginRegister
-          onLogin={this.handleLogin}
-          onLoginWithProvider={this.handleLoginWithProvider}
-          onRegister={this.handleRegister}
-          onRegisterWithProvider={this.handleRegisterWithProvider}
-          providers={[PROVIDER_GITHUB, PROVIDER_GOOGLE]}
-          disableLocal={this.state.disableLocal}
-          disableRegister={this.state.disableRegister}
-        />
-
-        {this.state.results.map(r => <Result key={r} message={r} />)}
-
-      </div>
-    );
-  }
-
-  handleLogin = content => {
-    this.addResult(`Logging in with ${JSON.stringify(content)}`)
-  }
-  handleLoginWithProvider = provider => {
-    this.addResult(`Logging in with provider=${provider}`);
-  }
-  handleRegister = content => {
-    this.addResult(`Registering with ${JSON.stringify(content)}`)
-  }
-  handleRegisterWithProvider = provider => {
-    this.addResult(`Registering with provider=${provider}`);
-  }
-
-  handleChange = name => event => {
-    this.setState({ [name]: event.target.checked });
-  };
-
-  addResult = msg => {
-    this.setState(prevState => {
-      return {
-        results: [...prevState.results, msg]
-      };
-    });
-  }
-}
-
-render(<App />, document.getElementById('root'));
-
-/* import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root')); */
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+)
