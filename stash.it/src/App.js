70983905b7715fda7f2879df6b949a0bff32067f
Brandon Desselle <desselle.brandon2@gmail.com>
2019-04-16 12:03:02 -0500

    Update App.js

--- stash.it/src/App.js
+++ stash.it/src/App.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+
+import './App.css'
+import Login from './components/Login'
+import Lists from './components/Lists'
+import PrivateRoute from './components/PrivateRoute'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className='container'>
-          {/*  */}
-        </div>
+        <Route exact path="/" component={Login} />
+        <PrivateRoute exact path="/tabs" component={Lists} />
       </div>
-    );
+    )
   }
 }
 
